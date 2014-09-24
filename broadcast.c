73d2239b1ad8016ea9a19ab3cbce86a55d9df955
Felix Ribeiro <ribeiro.felix@live.com>
2014-09-24 19:22:38 -0300

    a

--- broadcast.c
+++ broadcast.c
@@ -2,6 +2,7 @@
 #include "broadcast.h"
 #include <stdlib.h>
 #include <semaphore.h>
+#include <stdio.h>
 
 #define TAM 10
 
@@ -9,11 +10,13 @@
 
 int buffer[TAM];
 
-int proximaEscrita;
+int proximaEscrita = 0;
 
 int quantosLeram[TAM]; /* indica quantas threads leram uma posição */
+int receptoresEsperando[TAM];
 
 int totalReceptores = 0 ;
+int totalTransmissores = 0 ;
 
 /* variáveis por thread: */
 
@@ -23,51 +26,82 @@ int * correntes ; /* posição de leitura de cada thread destinatário */
 
 
 
-sem_t cons;
+sem_t cons[TAM];
 sem_t prod;
-int db;
-int dp;
+int dc = 0;
+int dp = 0 ;
 sem_t e;
 
 void incrementaCircular(int *);
 
 int inicia (int transmissores, int receptores)
 {
+	int i ;
 	correntes = (int*) malloc(sizeof(int)*receptores);
 	if (correntes == NULL)
 		return 0;
 
+	for ( i = 0; i < receptores; ++i)
+		correntes[i] = 0 ;
+
+	for (i = 0; i < TAM; ++i)
+	{
+		buffer[i] = 0;
+		quantosLeram[i] = 0 ;
+		receptoresEsperando[i] = 0;
+		sem_init(&cons[i],0,0);
+	}
+
+
 	totalReceptores = receptores;
+	totalTransmissores = transmissores;
 	
-	sem_init(&cons, 0, 0);
 	sem_init(&prod,0,0);
 	sem_init(&e,0,1);
-	
-	db = dp = 0;
-	
+
 	return 1;
 }
 
 void envia (int val)
 {
+	/* <await (B)S; > */
+	/* B =  quantosLeram[proximaEscrita] < totalReceptores       */
+	/* S = deposita item 																  */
+	/* <await (deposita[proximaEscrita] < totalReceptores ) >*/
+
+	//P(e)
 	sem_wait(&e);
-	if ( quantosLeram[proximaEscrita] == totalReceptores )
+	
+	// if (!B)
+	if ( quantosLeram[proximaEscrita] >= totalReceptores )
 	{
+		//dp++
 		dp++;
+		//V(e)
 		sem_post(&e);
+		printf("wait prod - envia \n");
+		//P(prod)
 		sem_wait(&prod);
+		printf("saiu wait prod - envia\n");
 	}
+
+	printf("antes coloca\n");
+
+	//S
 	buffer[proximaEscrita] = val;
 
+	printf("depois coloca\n");
+
 	//SIGNAL
-	if (dp > 0 )
+	if (dp > 0 && quantosLeram[proximaEscrita] < totalReceptores ) 
 	{
 		dp--;
+		printf("post prod\n");
 		sem_post(&prod);
-	}else if (db > 0 )
+	}else if ( receptoresEsperando[proximaEscrita] > 0 && quantosLeram[proximaEscrita] != totalReceptores  )
 	{
-		db--;
-		sem_post(&cons);
+		receptoresEsperando[proximaEscrita]--;
+		sem_post(&cons[proximaEscrita]);
 	}else
 		sem_post(&e);
 
@@ -77,37 +111,76 @@ int recebe (int meu_id)
 {
 	int corrente = correntes[meu_id];
 	int item; 
+	/* <await (B)S; > */
+	/* B =  proxEscrita > corrente && quantosLeram[corrente] != totalReceptores       */
+	/* S = pegar item 																  */
+	/* <await (proxEscrita > corrente && quantosLeram[corrente] != totalReceptores ) >*/
+
 	//P(e)
+	printf("wait e\n");
 	sem_wait(&e);
-	/* <await (proxEscrita > corrente) >*/
-	if (! proximaEscrita > corrente)
+	printf("saiu e\n");
+	
+	// if (!B)
+	if ( proximaEscrita == corrente && quantosLeram[corrente] == totalReceptores )
 	{
-		db++;
+		//db ++
+		receptoresEsperando[corrente]++;
 		sem_post(&e);
-		sem_wait(&cons);
+		//V(e)
+		printf("post e \n");
+		//P(sb)
+		sem_wait(&cons[corrente]);
+		printf("wait cons\n");
 	}
-	//S:
+
+		//S:
+	printf("critica %d \n", buffer[corrente]);
 	item = buffer[corrente];
+	printf("%d\n",item );	
+	correntes[meu_id] = (corrente + 1) % TAM;
+	
+	
 
-	incrementaCircular(&corrente);
-	correntes[meu_id] = corrente;
+	//SIGNAL
+	if (receptoresEsperando[corrente] > 0 )
+	{
+		receptoresEsperando[corrente]--;
+		sem_post(&cons[corrente]);
+	}else if (dp > 0 )
+	{
+		dp--;
+		printf("post prod em recebe\n");
+		sem_post(&prod);
+	}
+	else
+		sem_post(&e);
 
-		//SIGNAL
-	if (dp > 0 && corrente < proximaEscrita )
+
+
+	// <quantosLeram[corrente]++>
+	printf("oi1?\n");
+	sem_wait(&e);
+	printf("oi?\n");
+
+	quantosLeram[corrente]++;
+
+	//SIGNAL
+	if (totalTransmissores == 0 && proximaEscrita == corrente && quantosLeram[corrente] == totalReceptores && dp > 0  )
 	{
 		dp--;
 		sem_post(&prod);
-	}else if (db > 0 && corrente > 0 )
+	}else if ( receptoresEsperando[corrente] > 0 )
 	{
-		db--;
-		sem_post(&cons);
-	}else
+		receptoresEsperando[corrente] --;
+		sem_post(&cons[corrente]);
+		/* code */
+	}
+	else
 		sem_post(&e);
 
 
-	quantosLeram[corrente-1]++;
-
-	return 1;
+	return item;
 }
 
 
--- main.c
+++ main.c
@@ -13,31 +13,46 @@ void *w1()
 void * w2(void *x_void_ptr)
 {
 	int * val_ptr = (int*)x_void_ptr;
+	int item ;
 	printf("antes recebe\n");
-	*val_ptr = recebe(0);
-	printf("pos recebe\n");
+	item = recebe(0);
+	printf("pos recebe: %d \n",  item);
+
 	return NULL;
 }
 
 int main()
 {
-	int val;
+	int val = 0;
+	int val1 = 1;
+
 	printf("antes inicia\n");
 	inicia(1,1);
 
 
 	pthread_t inc_x_thread;
+	pthread_t inc_y_thread;
 
+	
+
+	
 	/* create a second thread which executes inc_x(&x) */
 	if(pthread_create(&inc_x_thread, NULL, w2, &val )) {
 
 		printf("Error creating thread\n");
 		return 1;
 	}
+	w1();
+	
+	// /* create a second thread which executes inc_x(&x) */
+	// if(pthread_create(&inc_y_thread, NULL, w2, &val1 )) {
+
+	// 	printf("Error creating thread\n");
+	// 	return 1;
+	// }
+	
 
 
-	printf("antes envia\n");
-	envia(3);
 
 	
 	return 0;
